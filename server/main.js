2fae27b4b21b2177ebf7a8a23ea6932681877990
Allan Niles <allancoding.dev@gmail.com>
2024-02-19 20:18:36 -0700

    save for the night

--- server/main.js
+++ server/main.js
@@ -2,15 +2,14 @@ var express = require('express');
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
+var morgan = require("morgan");
 
 var app = express();
+
+app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/startup', function(req, res){
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.use(function(req, res, next){
-  console.log(req.method, req.url);
-  next();
-});
 http.createServer(app).listen(80);
